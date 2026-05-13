e62d962306b349a27c74a47ac9a4d3a133cfeed1
datenwissenschaften <martin.franke@datenwissenschaften.com>
2026-05-13 19:06:29 +0200

    Add initial `.junie/memory` directory and regenerate `package-lock.json` to reflect new dependencies and project updates

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,24 +1,34 @@
-import { Component } from '@angular/core';
-import { AboutComponent } from '../about/about.component';
-import { ContactComponent } from '../contact/contact.component';
+import { AfterViewInit, Component, Renderer2 } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { NavbarComponent } from '../navbar/navbar.component';
-import { PortfolioComponent } from '../portfolio/portfolio.component';
 import { SectionComponent } from '../section/section.component';
 import { TitleComponent } from '../title/title.component';
 
 @Component({
     selector: 'app-home',
     imports: [
-        AboutComponent,
-        ContactComponent,
         FooterComponent,
         NavbarComponent,
-        PortfolioComponent,
         SectionComponent,
         TitleComponent,
     ],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss'
 })
-export class HomeComponent {}
+export class HomeComponent implements AfterViewInit {
+  constructor(private renderer: Renderer2) {}
+
+  ngAfterViewInit(): void {
+    this.loadCookieDeclarationScript();
+  }
+
+  loadCookieDeclarationScript(): void {
+    const script = this.renderer.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://consent.cookiebot.com/58891ae3-ac06-49f8-8f94-bef2aadb2dfe/cd.js';
+    script.id = 'CookieDeclaration';
+    script.async = true;
+    const cookieElement = this.renderer.selectRootElement('#cookies', true);
+    this.renderer.appendChild(cookieElement, script);
+  }
+}
